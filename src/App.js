81ad310e419a56fa3c7de836816964b05fce3b6a
M A Razvi <abuzarflw@gmail.com>
2022-05-31 02:12:27 -0700

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./Header";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Header/>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,47 @@
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img id="smallPic" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="#heroSection"
+                id="homeButton"
+              >
+                HOME
+              </a>
+              <a className="nav-link" href="#aboutSection">
+                ABOUT
+              </a>
+              <a className="nav-link" href="#workSection">
+                WORK
+              </a>
+              <a className="nav-link" href="#contactSection">
+                CONTACT
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
