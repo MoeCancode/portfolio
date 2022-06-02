2952f53b13959d7d5cb2ab3f2c4b6137974a670b
M A Razvi <abuzarflw@gmail.com>
2022-06-01 18:48:26 -0700

    Make collapsable nav menu for mobile devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import DP from "../assets/cropped.jpg";
+import {FaBars, FaTimes} from "react-icons/fa"
+import React from "react";
 
 const Header = () => {
+  const [navMode, setNavMode] = useState(false);
+
+  function toggleNav() {
+    setNavMode(!navMode);
+  }
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#000000] text-[#FFFFFF] font-bold">
       {/* My picture  */}
@@ -11,27 +20,28 @@ const Header = () => {
           style={{ width: "55px", height: "auto", borderRadius: "50%" }}
         />
       </div>
+      
       {/* Nav Links */}
-      <div>
-        <ul className="flex">
+        <ul className="hidden md:flex">
           <li>Home</li>
           <li>Skills</li>
           <li>Projects</li>
           <li>Contact</li>
           <li>Resume</li>
         </ul>
-      </div>
+      
       {/* Collapsed menu icon */}
-      <div>
-        {}
+      <div className="md:hidden z-10" onClick={toggleNav}>
+        {navMode ? <FaTimes/> : <FaBars/>}
       </div>
+      
       {/* Collapsed menu option */}
-      <ul className="hidden">
-          <li>Home</li>
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Contact</li>
-          <li>Resume</li>
+      <ul className={navMode ? "absolute top-0 left-0 w-full h-screen bg-[#1b0707] flex flex-col justify-center items-center": "hidden"}>
+          <li className="py-6 text-4xl">Home</li>
+          <li className="py-6 text-4xl">Skills</li>
+          <li className="py-6 text-4xl">Projects</li>
+          <li className="py-6 text-4xl">Contact</li>
+          <li className="py-6 text-4xl">Resume</li>
         </ul>
     </div>
   );
