1785825fb61d662343571aae380dc622718ebaf7
M A Razvi <abuzarflw@gmail.com>
2022-06-01 19:47:18 -0700

    Add links to socials and resume.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import DP from "../assets/cropped.jpg";
-import {FaBars, FaTimes} from "react-icons/fa"
-import React from "react";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaDiceFive,
+  FaFileAlt
+} from "react-icons/fa";
 
 const Header = () => {
   const [navMode, setNavMode] = useState(false);
@@ -20,29 +26,61 @@ const Header = () => {
           style={{ width: "55px", height: "auto", borderRadius: "50%" }}
         />
       </div>
-      
+
       {/* Nav Links */}
-        <ul className="hidden md:flex">
-          <li>Home</li>
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Contact</li>
-          <li>Resume</li>
-        </ul>
-      
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+        <li>Resume</li>
+      </ul>
+
       {/* Collapsed menu icon */}
       <div className="md:hidden z-10" onClick={toggleNav}>
-        {navMode ? <FaTimes/> : <FaBars/>}
+        {navMode ? <FaTimes /> : <FaBars />}
       </div>
-      
+
       {/* Collapsed menu option */}
-      <ul className={navMode ? "absolute top-0 left-0 w-full h-screen bg-[#1b0707] flex flex-col justify-center items-center": "hidden"}>
-          <li className="py-6 text-4xl">Home</li>
-          <li className="py-6 text-4xl">Skills</li>
-          <li className="py-6 text-4xl">Projects</li>
-          <li className="py-6 text-4xl">Contact</li>
-          <li className="py-6 text-4xl">Resume</li>
+      <ul
+        className={
+          navMode
+            ? "absolute top-0 left-0 w-full h-screen bg-[#1b0707] flex flex-col justify-center items-center"
+            : "hidden"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl">Resume</li>
+      </ul>
+
+      {/* Socials */}
+      <div className="flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#000000]" style={{color: "pink", border: "1px solid pink"}}>
+            <a href="https://github.com/MoeCancode/" className="flex justify-between items-center w-full">
+              GitHub <span style={{color: "white"}}><FaGithub size={30}/></span>
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#000000]" style={{color: "#2d7dd2", border: "1px solid #2d7dd2"}}>
+            <a href="https://www.linkedin.com/in/mohammad-razvi/" className="flex justify-between items-center w-full">
+              LinkedIn <span style={{color: "white"}}><FaLinkedin size={30}/></span>
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#000000]" style={{color: "#3e885b", border: "1px solid #3e885b"}}>
+            <a href="https://www.fiverr.com/abuzarrazvi?up_rollout=true" className="flex justify-between items-center w-full">
+              Fiverr <span style={{color: "white"}}><FaDiceFive size={30}/></span>
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#000000]" style={{color: "#C2714F", border: "1px solid #C2714F"}}>
+            <a href="https://drive.google.com/file/d/1yHR2GHoHAl7Lk1ZjDfdkKc5mvRDyIM4m/view?usp=sharing" className="flex justify-between items-center w-full">
+              Resume <span style={{color: "white"}}><FaFileAlt size={30}/></span>
+            </a>
+          </li>
         </ul>
+      </div>
     </div>
   );
 };
